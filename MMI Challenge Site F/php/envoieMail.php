6728a3a28d0f4205adbaf089b393c724375bd579
Adrien-SD <75619389+Adrien-SD@users.noreply.github.com>
2020-12-11 10:27:08 +0100

    Site Rewsound
    SIte Rewsound sans une partie des vidéos.

---
+++ MMI Challenge Site F/php/envoieMail.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+		<title>tableaux HTML</title>
+        <meta charset="utf-8" />		
+		<meta name="author" content="Jean-Luc Husson" />
+		<meta name="keywords" content="mail, php, query string" />
+		<meta name="description" content="Envoi d'un mail avec la fonction mail de php" />
+    </head>
+    
+    <body>
+  
+    
+<?php
+
+	// Récupération des champs qui sont censés être transmis
+	// en fait on récupère les valeurs associés aux noms des champs et 
+	// on les stocke dans des variables PHP (commencent par un $).
+
+	// Si les champs avaient été transmis en POST, il aurait fallu écrire $_POST
+	// à la place de $_GET.
+	
+	$destinataire = $_GET ['dest'] ;
+	$expediteur = $_GET ['exped'] ;
+	$sujet = $_GET ['sujet'] ;
+	$message = $_GET ['message'] ;
+	
+	// Ici on ne fait aucun contrôle, on espère que tous les champs sont bien remplis
+	// à la soumission du formulaire
+	
+	// Puis on appelle simplement la fonction mail() prédéfinie en php en lui transmettant 
+	// les paramètres qu'elle attend
+	
+	$headers = 'From: ' . $expediteur ; 
+	
+	mail ( $destinataire , $sujet , $message , $headers ) ;
+	
+
+
+?>    
+	
+	</body>
+	
+</html>
